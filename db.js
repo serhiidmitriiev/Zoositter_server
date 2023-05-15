aab5363402cc4bebacef1e3774dfb2c954dcbfc3
Serhii Dmitriiev <s.dmitriiev@andersenlab.com>
2023-05-15 17:10:13 +0400

    Connected to Atlas

--- db.js
+++ db.js
@@ -2,9 +2,12 @@ const { MongoClient } = require("mongodb");
 
 let dbConnection;
 
+let uri =
+  "mongodb+srv://dmitriievserguei:qTyu0UFnAxk6nmkG@cluster0.02vc3hp.mongodb.net/?retryWrites=true&w=majority";
+
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect("mongodb://127.0.0.1:27017/zoositter")
+    MongoClient.connect(uri)
       .then((client) => {
         dbConnection = client.db();
         return cb();
