2f5850889a78eaa612f4baa456476c0ef9a75a4f
Serhii Dmitriiev <s.dmitriiev@andersenlab.com>
2023-05-15 17:39:12 +0400

    Added request methods for owners

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ connectToDb((err) => {
   }
 });
 
-//routes
+///////////////////////Routes////////////////////////////
+
+//sitters
 app.get("/sitters", (req, res) => {
   const page = req.query.p || 0;
 
@@ -99,6 +101,89 @@ app.patch("/sitters/:id", (req, res) => {
 
   if (ObjectId.isValid(req.params.id)) {
     db.collection("sitters")
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: updates })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Couldn't update the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
+
+//owners
+app.get("/owners", (req, res) => {
+  const page = req.query.p || 0;
+
+  const ownersPerPage = 4;
+
+  let owners = [];
+
+  db.collection("owners")
+    .find()
+    .skip(page * ownersPerPage)
+    .limit(ownersPerPage)
+    .forEach((owner) => owners.push(owner))
+    .then(() => {
+      res.status(200).json(owners);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Couldn't fetch the documents" });
+    });
+});
+
+//Fetching a single document with owners
+
+app.get("/owners/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("owners")
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Couldn't fetch the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not valid document id" });
+  }
+});
+
+app.post("/owners", (req, res) => {
+  const owner = req.body;
+
+  db.collection("owners")
+    .insertOne(owner)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: "Could not create a new document" });
+    });
+});
+
+app.delete("/owners/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("owners")
+      .deleteOne({ _id: new ObjectId(req.params.id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Couldn't delete the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
+
+app.patch("/owners/:id", (req, res) => {
+  const updates = req.body;
+
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("owners")
       .updateOne({ _id: new ObjectId(req.params.id) }, { $set: updates })
       .then((result) => {
         res.status(200).json(result);
